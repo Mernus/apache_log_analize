3c901ee70d032a0bc9e9220450f1eb96cba48c5e
Mernus <mernusv@gmail.com>
2019-06-26 16:31:16 +0400

    tests

--- analizer/tests.py
+++ analizer/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from analizer.models import Logfile, Logdata
+from analizer.models import Logfile
 from django.db.models import Q
 from django.core import management
 
@@ -18,18 +18,21 @@ class ModelTest(TestCase):
                                          Q(added_datetime__minute__icontains='3'))
         response = self.client.get("/", {'q': 3})
         first_log = Logfile.objects.first()
-        sec_response = self.client.get("/" + str(first_log.pk) + "/", {'q': 132})
-        filtred_log = first_log.logdata_set.filter(Q(ip__icontains='132') |
-                                                   Q(datetime__year__icontains='132') |
-                                                   Q(datetime__month__icontains='132') |
-                                                   Q(datetime__day__icontains='132') |
-                                                   Q(datetime__hour__icontains='132') |
-                                                   Q(datetime__minute__icontains='132') |
-                                                   Q(http_method__icontains='132') |
-                                                   Q(requested_path__icontains='132') |
-                                                   Q(http_protocol__icontains='132') |
-                                                   Q(status_code__icontains='132') |
-                                                   Q(size_requested_obj__icontains='132') |
-                                                   Q(referer__icontains='132'))
         self.assertQuerysetEqual(logfile, map(repr, response.context['object_list']))
-        self.assertQuerysetEqual(filtred_log, map(repr, sec_response.context['object_list']))
+        sizes_sum = 0
+        ips = {}
+        for logdata in first_log.logdata_set.all():
+            try:
+                sizes_sum += int(logdata.size_requested_obj)
+            except ValueError:
+                pass
+            string_ip = str(logdata.ip)
+            if ips.get(string_ip) is not None:
+                ips[string_ip] += 1
+            else:
+                ips[string_ip] = 1
+        ips_list = sorted(ips.items(), key=lambda item: item[1], reverse=True)
+        ten_first_ip_list = [ip for ip in ips_list if ips_list.index(ip) < 10]
+        sec_response = self.client.get("/stats/" + str(first_log.pk) + "/")
+        self.assertEqual(sizes_sum, sec_response.context['sum_answer_size'])
+        self.assertListEqual(list(ten_first_ip_list), list(sec_response.context['first_ips']))
